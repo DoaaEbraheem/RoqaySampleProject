23cbe86f08cf7c429929f60c91c74ddca636ac4b
DoaaEbraheem <80466771+DoaaEbraheem@users.noreply.github.com>
2022-03-21 13:55:05 +0200

    setbases

---
+++ src/test/java/Samplebaseproject/Roqay/Screenshot.java
@@ -0,0 +1,43 @@
+package Samplebaseproject.Roqay;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Screenshot {
+	static String screenshotname="";
+	static Path dest = Paths.get("./Screenshots",screenshotname+".png");
+	public static void captureScreenshot(WebDriver driver , String screenshotname)  
+	{
+		 
+		try {
+			Files.createDirectories(dest.getParent());
+			FileOutputStream out = new FileOutputStream(dest.toString());
+			out.write(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES));
+			out.close();
+		} catch (IOException e) {
+			System.out.println("Excpetion while taking screenshot"+ e.getMessage());
+		}
+		
+	}
+	public static void CaptureFullScreen(WebDriver driver , String screenshotname) throws IOException {
+		java.io.File source= ((FirefoxDriver)driver).getFullPageScreenshotAs(OutputType.FILE);
+		org.openqa.selenium.io.FileHandler.copy(source, new java.io.File(dest.toString()));
+	}
+	public static void TakeElementScreenShot(WebDriver driver , String screenshotname,String Cssselector) throws IOException {
+		WebElement elem=driver.findElement(By.cssSelector(Cssselector));
+		java.io.File source=elem.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(source,  new java.io.File(dest.toString()));
+		
+	}
+
+}
---
+++ src/test/java/Samplebaseproject/Roqay/TestBase.java
@@ -0,0 +1,94 @@
+package Samplebaseproject.Roqay;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.asserts.SoftAssert;
+
+import com.applitools.eyes.MatchLevel;
+import com.applitools.eyes.selenium.Eyes;
+import com.github.javafaker.Faker;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class TestBase {
+	protected static WebDriver driver;
+	protected static Eyes eyes;
+	public static Properties prop;
+	public Faker faker = new Faker();
+	//LoginPage loginpageObject;
+	public static FileInputStream fis;
+	// soft assertion method
+	protected SoftAssert softassert = new SoftAssert();
+
+
+
+	// Initiate the Chrome browser
+	@BeforeSuite
+	public void openBrowser() throws InterruptedException, Exception {
+		
+		prop = new Properties();
+		try {
+			fis = new FileInputStream("resources\\test.properties");
+			prop.load(fis);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		 initiEyes();
+		WebDriverManager.chromedriver().setup();
+	    driver=new ChromeDriver();
+	   
+	    driver.manage().window().maximize();
+	    
+		driver.get(prop.getProperty("url"));
+		
+		driver.manage().deleteAllCookies();
+	
+	}
+	
+
+	// Initiate Eye apptlitool for visual testing
+	private static void initiEyes() {
+		eyes = new Eyes();
+		eyes.setApiKey(prop.getProperty("applitools.api.key"));
+
+	}
+
+
+	// validate window design and elements
+	public void validateWindow() {
+		eyes.open(driver, "Easy buy", Thread.currentThread().getStackTrace()[2].getMethodName());
+		
+		eyes.setMatchLevel(MatchLevel.CONTENT);
+		eyes.setForceFullPageScreenshot(true);
+		eyes.checkWindow();
+		eyes.close();
+	}
+
+	// taking screenshot for failures
+	@AfterMethod
+	public void screenshotOnFailure(ITestResult result) {
+		if (result.getStatus() == ITestResult.FAILURE) {
+			System.out.println("Failed!");
+			System.out.println("Taking Screenshot....");
+			Screenshot.captureScreenshot(driver, result.getName());
+		}
+	}
+
+	// close browser
+	@AfterSuite
+	public void quitBrowser() {
+		driver.quit();
+		eyes.abortIfNotClosed();
+	}
+
+}
+
